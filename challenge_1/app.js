bdf30781302855720f5c2c908e980079e3be56a5
Michael Lee <michaellee@Michaels-MacBook-Pro-2.local>
2019-08-12 16:29:03 -0700

    Commit prior to refactor code into modules

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,7 +1,15 @@
+//game state on load
 var player = true;
 var playerX = [];
 var playerO = [];
+var name1 = '';
+var name2 = '';
+var player1Score = 0;
+var player2Score = 0;
+document.getElementById('player1').innerHTML = `${name1} X: ${player1Score}`;
+document.getElementById('player2').innerHTML = `${name2} O: ${player2Score}`;
 
+//verify win
 var verify = (arr, coord) => {
     var horiz = 1;
     var vert = 1;
@@ -32,6 +40,7 @@ var verify = (arr, coord) => {
     return false;
 }
 
+// game functionality
 var click = (event) => {
     if (event.target.innerHTML) {
         return;
@@ -47,21 +56,58 @@ var click = (event) => {
     event.target.append(document.createTextNode(`${piece}`));
     if (player) {
         if (verify(playerX, coord)) {
-            document.getElementById('winner').innerHTML = 'Player X Wins!';
+            if (name1 === '') {
+                document.getElementById('winner').innerHTML = `X Wins!`;
+            } else {
+                document.getElementById('winner').innerHTML = `${name1} Wins!`;
+            }
+            player1Score++;
+            document.getElementById('player1').innerHTML = `${name1} X: ${player1Score}`;
+            player = true;
+            return;
         }
         playerX.push(coord);
     } else {
         if (verify(playerO, coord)) {
-            document.getElementById('winner').innerHTML = 'Player O Wins!';
+            if (name2 === '') {
+                document.getElementById('winner').innerHTML = `O Wins!`;
+            } else {
+                document.getElementById('winner').innerHTML = `${name2} Wins!`;
+            }
+            player2Score++;
+            document.getElementById('player2').innerHTML = `${name2} O: ${player2Score}`;
+            player = false;
+            return;
         }
         playerO.push(coord);
     }
     player = !player;
 }
 
+//game board click handlers
 for (var i = 1; i <= 9; i++) {
     document.getElementById(`${i}`).addEventListener('click', (event) => {click(event)})
 }
+
+//name selection click handlers
+document.getElementById('submit1').addEventListener('click', () => {
+    if (document.querySelector('#name1').value === '') {
+        return;
+    }
+    name1 = document.querySelector('#name1').value;
+    document.getElementById('player1').innerHTML = `${name1} X: ${player1Score}`;
+    document.querySelector('#name1').value = '';
+})
+document.getElementById('submit2').addEventListener('click', () => {
+    if (document.querySelector('#name2').value === '') {
+        return;
+    }
+    name2 = document.querySelector('#name2').value;
+    document.getElementById('player2').innerHTML = `${name2} O: ${player2Score}`;
+    document.querySelector('#name2').value = '';
+})
+
+//reset click handler
 document.getElementById('reset').addEventListener('click', () => {
     for (var j = 1; j <= 9; j++) {
         document.getElementById(`${j}`).innerHTML = '';
@@ -69,5 +115,4 @@ document.getElementById('reset').addEventListener('click', () => {
     document.getElementById('winner').innerHTML = '';
     playerX = [];
     playerO = [];
-    player = true;
 })
