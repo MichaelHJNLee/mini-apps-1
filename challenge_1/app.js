cd5dcc23cd9ff8c9be330cd952b9e805468a1051
Michael Lee <michaellee@Michaels-MacBook-Pro-2.local>
2019-08-12 17:20:24 -0700

    Refactored code into modules

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,118 +1,131 @@
-//game state on load
-var player = true;
-var playerX = [];
-var playerO = [];
-var name1 = '';
-var name2 = '';
-var player1Score = 0;
-var player2Score = 0;
-document.getElementById('player1').innerHTML = `${name1} X: ${player1Score}`;
-document.getElementById('player2').innerHTML = `${name2} O: ${player2Score}`;
-
-//verify win
-var verify = (arr, coord) => {
-    var horiz = 1;
-    var vert = 1;
-    var diag = 1;
-    for (var i = 0; i < arr.length; i++) {
-        if (arr[i][0] === coord[0]) {
-            horiz++;
-        }
-        if (arr[i][1] === coord[1]) {
-            vert++;
-        }
-        if (arr[i][0] === coord[0] + 1 && arr[i][1] === coord[1] + 1) {
-            diag++;
-        }
-        if (arr[i][0] === coord[0] - 1 && arr[i][1] === coord[1] - 1) {
-            diag++;
-        }
-        if (arr[i][0] === coord[0] + 2 && arr[i][1] === coord[1] + 2) {
-            diag++;
-        }
-        if (arr[i][0] === coord[0] - 2 && arr[i][1] === coord[1] - 2) {
-            diag++;
-        }
-    }
-    if (horiz === 3 || vert === 3 || diag === 3) {
-        return true;
-    }
-    return false;
+//Model: holds game state
+var Model = {
+    name1: '',
+    name2: '',
+    player: true,
+    playerX: [],
+    playerO: [],
+    player1Score: 0,
+    player2Score: 0,
 }
 
-// game functionality
-var click = (event) => {
-    if (event.target.innerHTML) {
-        return;
-    }
-    var bank = [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]];
-    var coord = bank[parseInt(event.target.id) - 1];
-    var piece;
-    if (player) {
-        piece = 'x';
-    } else {
-        piece = 'o';
-    }
-    event.target.append(document.createTextNode(`${piece}`));
-    if (player) {
-        if (verify(playerX, coord)) {
-            if (name1 === '') {
-                document.getElementById('winner').innerHTML = `X Wins!`;
-            } else {
-                document.getElementById('winner').innerHTML = `${name1} Wins!`;
+//View: holds game functionality; dom manipulation
+var View = {
+    initialize: () => {
+        document.getElementById('player1').innerHTML = `${Model.name1} X: ${Model.player1Score}`;
+        document.getElementById('player2').innerHTML = `${Model.name2} O: ${Model.player2Score}`;
+    },
+    verify: (arr, coord) => {
+        var horiz = 1;
+        var vert = 1;
+        var diag = 1;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i][0] === coord[0]) {
+                horiz++;
             }
-            player1Score++;
-            document.getElementById('player1').innerHTML = `${name1} X: ${player1Score}`;
-            player = true;
+            if (arr[i][1] === coord[1]) {
+                vert++;
+            }
+            if (arr[i][0] === coord[0] + 1 && arr[i][1] === coord[1] + 1) {
+                diag++;
+            }
+            if (arr[i][0] === coord[0] - 1 && arr[i][1] === coord[1] - 1) {
+                diag++;
+            }
+            if (arr[i][0] === coord[0] + 2 && arr[i][1] === coord[1] + 2) {
+                diag++;
+            }
+            if (arr[i][0] === coord[0] - 2 && arr[i][1] === coord[1] - 2) {
+                diag++;
+            }
+        }
+        if (horiz === 3 || vert === 3 || diag === 3) {
+            return true;
+        }
+        return false;
+    },
+    click: (event) => {
+        if (event.target.innerHTML) {
             return;
         }
-        playerX.push(coord);
-    } else {
-        if (verify(playerO, coord)) {
-            if (name2 === '') {
-                document.getElementById('winner').innerHTML = `O Wins!`;
-            } else {
-                document.getElementById('winner').innerHTML = `${name2} Wins!`;
+        var bank = [[0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1], [2,2]];
+        var coord = bank[parseInt(event.target.id) - 1];
+        if (Model.player) {
+            event.target.append(document.createTextNode('x'));
+            if (View.verify(Model.playerX, coord)) {
+                if (Model.name1 === '') {
+                    document.getElementById('winner').innerHTML = `X Wins!`;
+                } else {
+                    document.getElementById('winner').innerHTML = `${Model.name1} Wins!`;
+                }
+                Model.player1Score++;
+                document.getElementById('player1').innerHTML = `${Model.name1} X: ${Model.player1Score}`;
+                Model.player = true;
+                return;
             }
-            player2Score++;
-            document.getElementById('player2').innerHTML = `${name2} O: ${player2Score}`;
-            player = false;
-            return;
+            Model.playerX.push(coord);
+        } else {
+            event.target.append(document.createTextNode('o'));
+            if (View.verify(Model.playerO, coord)) {
+                if (Model.name2 === '') {
+                    document.getElementById('winner').innerHTML = `O Wins!`;
+                } else {
+                    document.getElementById('winner').innerHTML = `${Model.name2} Wins!`;
+                }
+                Model.player2Score++;
+                document.getElementById('player2').innerHTML = `${Model.name2} O: ${Model.player2Score}`;
+                Model.player = false;
+                return;
+            }
+            Model.playerO.push(coord);
         }
-        playerO.push(coord);
+        Model.player = !Model.player;
+    },
+    nameSelect: (num) => {
+        if (num === 1) {
+            if (document.querySelector('#name1').value === '') {
+                return;
+            }
+            Model.name1 = document.querySelector('#name1').value;
+            document.getElementById('player1').innerHTML = `${Model.name1} X: ${Model.player1Score}`;
+            document.querySelector('#name1').value = '';
+        } else {
+            if (document.querySelector('#name2').value === '') {
+                return;
+            }
+            Model.name2 = document.querySelector('#name2').value;
+            document.getElementById('player2').innerHTML = `${Model.name2} O: ${Model.player2Score}`;
+            document.querySelector('#name2').value = '';
+        }
+    },
+    reset: () => {
+        for (var j = 1; j <= 9; j++) {
+            document.getElementById(`${j}`).innerHTML = '';
+        }
+        document.getElementById('winner').innerHTML = '';
+        Model.playerX = [];
+        Model.playerO = [];
     }
-    player = !player;
 }
 
-//game board click handlers
-for (var i = 1; i <= 9; i++) {
-    document.getElementById(`${i}`).addEventListener('click', (event) => {click(event)})
+//Controller: handles user input; click handlers
+var Controller = {
+    boardClick: () => {
+        for (var i = 1; i <= 9; i++) {
+            document.getElementById(`${i}`).addEventListener('click', (event) => {View.click(event)})
+        }
+    },
+    nameSelect: () => {
+        document.getElementById('submit1').addEventListener('click', () => {View.nameSelect(1)})
+        document.getElementById('submit2').addEventListener('click', () => {View.nameSelect(2)})
+    },
+    reset: () => {
+        document.getElementById('reset').addEventListener('click', () => {View.reset()});
+    }
 }
 
-//name selection click handlers
-document.getElementById('submit1').addEventListener('click', () => {
-    if (document.querySelector('#name1').value === '') {
-        return;
-    }
-    name1 = document.querySelector('#name1').value;
-    document.getElementById('player1').innerHTML = `${name1} X: ${player1Score}`;
-    document.querySelector('#name1').value = '';
-})
-document.getElementById('submit2').addEventListener('click', () => {
-    if (document.querySelector('#name2').value === '') {
-        return;
-    }
-    name2 = document.querySelector('#name2').value;
-    document.getElementById('player2').innerHTML = `${name2} O: ${player2Score}`;
-    document.querySelector('#name2').value = '';
-})
+View.initialize();
+Controller.boardClick();
+Controller.nameSelect();
+Controller.reset();
 
-//reset click handler
-document.getElementById('reset').addEventListener('click', () => {
-    for (var j = 1; j <= 9; j++) {
-        document.getElementById(`${j}`).innerHTML = '';
-    }
-    document.getElementById('winner').innerHTML = '';
-    playerX = [];
-    playerO = [];
-})
