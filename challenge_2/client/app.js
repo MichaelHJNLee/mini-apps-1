05b3f22ac4f69237dbec86ea665d093f83101572
Michael Lee <michaellee@Michaels-MacBook-Pro-2.local>
2019-08-13 22:20:27 -0700

    refactor

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,17 +1,17 @@
-$('.submit').on('click', () => {
-    var formData = new FormData();
-    var request = new XMLHttpRequest();
-    formData.append('file', document.getElementById('file').files[0]);
-    request.open("POST", 'localhost:3000')
-    request.send(formData);
-    // $.ajax({
-    //     url: 'localhost:3000',
-    //     method: 'POST',
-    //     data: document.getElementById('text').value,
-    //     success: (data) => {
-    //         console.log(data)
-    //         $('.hold').append(data)
-    //     }
-    // })
-})
+// $('.submit').on('click', () => {
+//     var formData = new FormData();
+//     var request = new XMLHttpRequest();
+//     formData.append('file', document.getElementById('file').files[0]);
+//     request.open("POST", 'localhost:3000')
+//     request.send(formData);
+//     // $.ajax({
+//     //     url: 'localhost:3000',
+//     //     method: 'POST',
+//     //     data: document.getElementById('text').value,
+//     //     success: (data) => {
+//     //         console.log(data)
+//     //         $('.hold').append(data)
+//     //     }
+//     // })
+// })
 
